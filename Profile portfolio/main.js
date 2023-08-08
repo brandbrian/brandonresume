9a43e4d3b1b73dd2008bded8f0c320c91e0be564
brandbrian <139859279+brandbrian@users.noreply.github.com>
2023-08-08 21:12:36 +0800

    Add files via upload

---
+++ Profile portfolio/main.js
@@ -0,0 +1,9 @@
+var checkbox = document.querySelector('#checkbox');
+
+checkbox.addEventListener('change', function() {
+    if (this.checked) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+});
